f8be0b46b4e44e4e0ce956a2595cfaba6ec163ee
girogio <giorgiogrigolo03@gmail.com>
2025-02-10 01:04:37 +0100

    Build script moves dll automatically

--- ascendio-app/src-tauri/build.rs
+++ ascendio-app/src-tauri/build.rs
@@ -1,3 +1,14 @@
+use std::fs::copy;
+use std::path::Path;
+
+const PATH_LIB: &str = "lib";
+const LIBRARIES: [&str; 1] = ["SimConnect.dll"];
+
 fn main() {
-    tauri_build::build()
+    for lib in LIBRARIES.iter() {
+        let path = Path::new(PATH_LIB).join(lib);
+        copy(path, format!("target/debug/{}", lib)).unwrap();
+    }
+
+    tauri_build::build();
 }
